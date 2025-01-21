7681face1352072c0d5e32fd3abaee00c4527ed7
Parvesh <parvesh.rana.official@gmail.com>
2025-01-21 21:47:29 +0530

    initial 2 commit

--- shop/urls.py
+++ shop/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("", views.index, name="home" ),
+    path("product/<str:product_name>/", views.product_detail, name="product_detail" ),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,15 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import ProductCat,Product,ProductImage,HomepageBanner
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     banners=HomepageBanner.objects.all()
-    print(banners)
     best=Product.objects.order_by('bestselling_rank')[:4]
-    print(best)
     return render(request, 'homepage.html',{
         'banners':banners,
         'best':best,
     })
+def product_detail(request,product_name):
+    product = get_object_or_404(Product, sku=product_name)
+    print(product)
+    images = ProductImage.objects.filter(sku=product)
+    return render(request, 'product.html',{'product':product,'images':images})
    
